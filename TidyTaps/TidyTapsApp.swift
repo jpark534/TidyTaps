50b36aa36706702eca9d13c1e895251025fa9b70
Julia Park <155590082+jpark534@users.noreply.github.com>
2025-08-10 01:59:32 -0400

    starting app at HomepageView

--- TidyTaps/TidyTapsApp.swift
+++ TidyTaps/TidyTapsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TidyTapsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomepageView()  //starting point
         }
     }
 }
